f2960ffa6153414b172fa8bde93dad8b98c2636e
raksmey <raksmeyrun5@gmail.com>
2023-03-06 11:24:31 +0700

    config mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import mongodbConfig from './config/database/mongodb.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [mongodbConfig],
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    // MongooseModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: async (config: ConfigService) => ({
+    //     uri: config.get<string>('mongodb.uri'), // Loaded from .ENV
+    //   }),
+    // }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'), // Loaded from .ENV
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => console.log(`Running on port 3000...`));
+  const config: ConfigService = app.get(ConfigService);
+  const port = parseInt(process.env.PORT, 10);
+  await app.listen(config.get<number>('port'), () =>
+    console.log(`Running on port ${port}...`),
+  );
+  // await app.listen(3000, () => console.log(`Running on port 3000...`));
 }
-bootstrap();
+bootstrap().then(() => console.log('Success'));
