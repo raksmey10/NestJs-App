6e746b7344401cad8b2d8df82d2121401ec1a43c
raksmey <raksmeyrun5@gmail.com>
2023-03-06 14:03:56 +0700

    remove config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,36 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import mongodbConfig from './config/database/mongodb.config';
+// import mongodbConfig from './config/database/mongodb.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [mongodbConfig],
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        uri: config.get<string>('mongodb.uri'), // Loaded from .ENV
-      }),
-    }),
-    // MongooseModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: async (config: ConfigService) => ({
-    //     uri: config.get<string>('MONGODB_URI'), // Loaded from .ENV
-    //   }),
-    // }),
+    MongooseModule.forRoot(
+      'mongodb+srv://raksmey:lBzwRHZG6Yr333FN@cluster0.dhjzjhp.mongodb.net/myapp',
+    ),
   ],
+  // ConfigModule.forRoot({
+  //   load: [mongodbConfig],
+  //   isGlobal: true,
+  //   envFilePath: '.env',
+  // }),
+  // MongooseModule.forRootAsync({
+  //   imports: [ConfigModule],
+  //   inject: [ConfigService],
+  //   useFactory: async (config: ConfigService) => ({
+  //     uri: config.get<string>('mongodb.uri'), // Loaded from .ENV
+  //   }),
+  // }),
+  // MongooseModule.forRootAsync({
+  //   imports: [ConfigModule],
+  //   inject: [ConfigService],
+  //   useFactory: async (config: ConfigService) => ({
+  //     uri: config.get<string>('MONGODB_URI'), // Loaded from .ENV
+  //   }),
+  // }),
+
   controllers: [AppController],
   providers: [AppService],
 })
