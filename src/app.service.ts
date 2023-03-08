e672119ad87c55bf6d32e0d122cc26c6e357d0bc
raksmey <raksmeyrun5@gmail.com>
2023-03-08 15:41:59 +0700

    test docker run -d

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    // return 'Hello World!';
+    const port = parseInt(process.env.PORT);
+    return `app service running on port ${port}...`;
   }
 }
